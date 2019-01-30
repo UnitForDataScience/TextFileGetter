431b7ea2591cde379c2304386f7a44a8062dfff1
shanky <shashank.prof.05@gmail.com>
2019-01-30 12:49:18 -0700

    some changed

--- MozillaTextExtractor/borderify.js
+++ MozillaTextExtractor/borderify.js
@@ -1,9 +1,9 @@
 var body = document.body;
 var fileName = window.location.href.split('https://lersearch.inl.gov/TextView.aspx?DOC::')[1].split('.TXT')[0];
 var text = body.textContent;
-var encodedUri = encodeURI(text);
+var encodedUri = text;
 var link = document.createElement("a");
-link.setAttribute("href", 'data:text/csv;charset=utf-8,' + encodedUri);
-link.setAttribute("download", fileName + '.csv');
+link.setAttribute("href", 'data:text/csv;charset=utf-8,' + escape(encodedUri));
+link.setAttribute("download", fileName + '.txt');
 document.body.appendChild(link);
 link.click();
